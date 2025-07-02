05cf249806432ed567adeed28bab251d828a5e03
unknown <jwon96.park@gmail.com>
2025-07-02 10:04:43 +0900

    [refactoring] page 46

--- PrimeFactor/prime-factors_test.cpp
+++ PrimeFactor/prime-factors_test.cpp
@@ -1,14 +1,19 @@
 #include "gmock/gmock.h"
 #include "prime-factors.cpp"
 
-TEST(PrimeFactors, Of1) {
+using namespace testing;
+class PrimeFixture : public Test {
+public:
 	PrimeFactors prime_factor;
+	vector<int> expected;
+};
+
+TEST_F(PrimeFixture, Of1) {
 	vector<int> exepcted = {};
 	EXPECT_EQ(exepcted, prime_factor.of(1));
 }
 
-TEST(PrimeFactors, Of2) {
-	PrimeFactors prime_factor;
+TEST_F(PrimeFixture, Of2) {
 	vector<int> exepcted = {2};
 	EXPECT_EQ(exepcted, prime_factor.of(2));
 }
