2ab9d7f1f448eba21e959a5013b8d2df0990ab96
unknown <jwon96.park@gmail.com>
2025-07-02 09:58:49 +0900

    [feature] page 41

--- PrimeFactor/prime-factors_test.cpp
+++ PrimeFactor/prime-factors_test.cpp
@@ -1,6 +1,8 @@
 #include "gmock/gmock.h"
 #include "prime-factors.cpp"
 
-TEST(TS, TC1) {
-	EXPECT_EQ(1, 1);
+TEST(PrimeFactors, Of1) {
+	PrimeFactors prime_factor;
+	vector<int> exepcted = {};
+	EXPECT_EQ(exepcted, prime_factor.of(1));
 }
